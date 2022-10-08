e5049830f1a01254e7ea0406d7e6f4ca236d5150
Ajay KC <meajay64@gmail.com>
2022-10-09 02:07:17 +0900

    Add logic and styles for theme 3

--- index.js
+++ index.js
@@ -106,44 +106,125 @@ toggleButtons.forEach((btn, index) => {
       }
     }
 
+    if (calculatorObj.theme === "one") {
+      let themeBox = document.querySelector(".theme__box");
+      let main = document.querySelector("main");
+      let headerLeft = document.querySelector(".header__left");
+      let headerRight = document.querySelector(".header__right");
+      let calculatorScreen = document.querySelector(".calculator__screen");
+      let calculatorKeypad = document.querySelector(".calculator__keypad");
+      let deleteBtn = document.querySelector(".btn__delete");
+      let resetBtn = document.querySelector(".btn__reset");
+      let equalBtn = document.querySelector(".btn__equal");
+      let calculatorBtnList = document.querySelectorAll(".calculator__btn");
+
+      let elements = [
+        themeBox,
+        main,
+        headerLeft,
+        headerRight,
+        calculatorScreen,
+        calculatorKeypad,
+        deleteBtn,
+        resetBtn,
+        equalBtn,
+      ];
+
+      elements.forEach((item) => {
+        item.classList.remove("theme2");
+        item.classList.remove("theme3");
+      });
+
+      calculatorBtnList.forEach((btn) => {
+        btn.classList.remove("theme2");
+        btn.classList.remove("theme3");
+      });
+    }
+
     if (calculatorObj.theme === "two") {
       let themeBox = document.querySelector(".theme__box");
-      let main = document.querySelector('main');
-      let headerLeft = document.querySelector('.header__left');
-      let headerRight = document.querySelector('.header__right');
-      let calculatorScreen = document.querySelector('.calculator__screen');
-      let calculatorKeypad = document.querySelector('.calculator__keypad');
-      let deleteBtn = document.querySelector('.btn__delete'); 
-      let resetBtn = document.querySelector('.btn__reset'); 
-      let equalBtn = document.querySelector('.btn__equal'); 
+      let main = document.querySelector("main");
+      let headerLeft = document.querySelector(".header__left");
+      let headerRight = document.querySelector(".header__right");
+      let calculatorScreen = document.querySelector(".calculator__screen");
+      let calculatorKeypad = document.querySelector(".calculator__keypad");
+      let deleteBtn = document.querySelector(".btn__delete");
+      let resetBtn = document.querySelector(".btn__reset");
+      let equalBtn = document.querySelector(".btn__equal");
 
-      themeBox.classList.remove("theme");
+      themeBox.classList.remove("theme3");
       themeBox.classList.add("theme2");
       nextSibling.classList.add("theme2");
 
       main.classList.remove("theme3");
       main.classList.add("theme2");
 
-      headerLeft.classList.remove('theme3')
-      headerLeft.classList.add('theme2');
+      headerLeft.classList.remove("theme3");
+      headerLeft.classList.add("theme2");
 
-      headerRight.classList.remove('theme3')
-      headerRight.classList.add('theme2');
+      headerRight.classList.remove("theme3");
+      headerRight.classList.add("theme2");
 
-      calculatorScreen.classList.remove('theme3')
-      calculatorScreen.classList.add('theme2');
+      calculatorScreen.classList.remove("theme3");
+      calculatorScreen.classList.add("theme2");
 
-      calculatorKeypad.classList.remove('theme3')
-      calculatorKeypad.classList.add('theme2');
+      calculatorKeypad.classList.remove("theme3");
+      calculatorKeypad.classList.add("theme2");
 
-      deleteBtn.classList.remove('theme3')
-      deleteBtn.classList.add('theme2');
+      deleteBtn.classList.remove("theme3");
+      deleteBtn.classList.add("theme2");
 
-      resetBtn.classList.remove('theme3')
-      resetBtn.classList.add('theme2');
+      resetBtn.classList.remove("theme3");
+      resetBtn.classList.add("theme2");
 
-      equalBtn.classList.remove('theme3')
-      equalBtn.classList.add('theme2');
+      equalBtn.classList.remove("theme3");
+      equalBtn.classList.add("theme2");
+    }
+
+    if (calculatorObj.theme === "three") {
+      let themeBox = document.querySelector(".theme__box");
+      let main = document.querySelector("main");
+      let headerLeft = document.querySelector(".header__left");
+      let headerRight = document.querySelector(".header__right");
+      let calculatorScreen = document.querySelector(".calculator__screen");
+      let calculatorKeypad = document.querySelector(".calculator__keypad");
+      let deleteBtn = document.querySelector(".btn__delete");
+      let resetBtn = document.querySelector(".btn__reset");
+      let equalBtn = document.querySelector(".btn__equal");
+      let calculatorBtnList = document.querySelectorAll(".calculator__btn");
+
+      themeBox.classList.remove("theme2");
+      themeBox.classList.add("theme3");
+      nextSibling.classList.add("theme3");
+
+      main.classList.remove("theme2");
+      main.classList.add("theme3");
+
+      headerLeft.classList.remove("theme2");
+      headerLeft.classList.add("theme3");
+
+      headerRight.classList.remove("theme2");
+      headerRight.classList.add("theme3");
+
+      calculatorScreen.classList.remove("theme2");
+      calculatorScreen.classList.add("theme3");
+
+      calculatorKeypad.classList.remove("theme2");
+      calculatorKeypad.classList.add("theme3");
+
+      deleteBtn.classList.remove("theme2");
+      deleteBtn.classList.add("theme3");
+
+      resetBtn.classList.remove("theme2");
+      resetBtn.classList.add("theme3");
+
+      equalBtn.classList.remove("theme2");
+      equalBtn.classList.add("theme3");
+
+      calculatorBtnList.forEach((btn) => {
+        btn.classList.remove("theme2");
+        btn.classList.add("theme3");
+      });
     }
   });
 });
